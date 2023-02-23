971378813f6f3d612e6e212ee3acb259b2faa45d
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-02-23 13:36:13 -0300

    replace file html to template in in ts file

--- src/app/layout/main/main.component.ts
+++ src/app/layout/main/main.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.sass']
+	selector: 'app-main',
+	template: `
+	<app-header></app-header>
+		<div class="container">
+			<router-outlet></router-outlet>
+		</div>
+	<app-footer></app-footer>`,
+	styleUrls: ['./main.component.sass']
 })
 export class MainComponent {
 
