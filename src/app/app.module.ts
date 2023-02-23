ef6b24b0fefe8bb725f1267bca1269943a5c7db7
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-02-23 10:46:00 -0300

    location and path location strategy added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
 import { MainComponent } from './layout/main/main.component';
+import { FooterComponent } from './layout/footer/footer.component';
+
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    MainComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		MainComponent,
+		FooterComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [
+		{
+			provide: LocationStrategy,
+			useClass: PathLocationStrategy
+		}
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
