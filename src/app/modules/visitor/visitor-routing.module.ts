2989ae32c414707e16972fa86272ba7483f504d8
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-02-23 10:50:49 -0300

    visitor routing added

--- src/app/modules/visitor/visitor-routing.module.ts
+++ src/app/modules/visitor/visitor-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VisitorComponent } from '@modules/visitor/page/visitor/visitor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		component: VisitorComponent,
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
 })
 export class VisitorRoutingModule { }
