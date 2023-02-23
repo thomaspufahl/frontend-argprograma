20259bbb67785661c59d6d1918e567cb7d087486
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-02-23 10:43:36 -0300

    visitor page component added

--- src/app/modules/visitor/visitor.module.ts
+++ src/app/modules/visitor/visitor.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VisitorRoutingModule } from './visitor-routing.module';
+import { VisitorComponent } from './page/visitor/visitor.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    VisitorComponent
+  ],
   imports: [
     CommonModule,
     VisitorRoutingModule
