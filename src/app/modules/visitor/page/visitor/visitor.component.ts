f963cb88b669f7e43623f59547f33531cd4784e5
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-04-20 16:41:34 -0300

    remove code with user service(removed)

--- src/app/modules/visitor/page/visitor/visitor.component.ts
+++ src/app/modules/visitor/page/visitor/visitor.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService, User } from '@core/http/user/user.service';
 @Component({
 	selector: 'app-visitor',
 	templateUrl: './visitor.component.html',
@@ -7,27 +6,8 @@ import { UserService, User } from '@core/http/user/user.service';
 })
 export class VisitorComponent implements OnInit {
 
-	users: User[] = [];
-
-	constructor(private readonly userService: UserService) { }
-
 	ngOnInit(): void {
-		this.getUsers();
-		console.log(this.users);
-	}
 
-	public getUsers() {
-		this.userService.getUsers().subscribe((users: any) => {
-			this.users = [...users];
-		});
-	}
-
-	public createUser(username: String, password: String) {
-		this.userService.createUser(username, password);
-	}
-
-	public deleteAllUsers() {
-		this.userService.deleteAllUsers();
 	}
 
 }
