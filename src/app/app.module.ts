63316fcbb5844d2124d40df1658a7cd05f718449
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-04-20 16:38:14 -0300

    added coreModule to imports in appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CoreModule } from '@core/core.module';
 import { HeaderComponent } from './layout/header/header.component';
 import { MainComponent } from './layout/main/main.component';
 import { FooterComponent } from './layout/footer/footer.component';
@@ -20,7 +21,8 @@ import { LocationStrategy, PathLocationStrategy } from '@angular/common';
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+		CoreModule
 	],
 	providers: [
 		{
