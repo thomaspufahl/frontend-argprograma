50c622a33c309a874384cf132cae3cf86ec066da
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-04-22 18:19:01 -0300

    modified profile component route to @shared

--- src/app/modules/visitor/visitor.module.ts
+++ src/app/modules/visitor/visitor.module.ts
@@ -7,7 +7,7 @@ import { VisitorRoutingModule } from './visitor-routing.module';
 import { VisitorComponent } from './page/visitor/visitor.component';
 
 
-import { ProfileComponent } from './components/profile/profile.component';
+import { ProfileComponent } from '@shared/components/profile/profile.component';
 import { LoginComponent } from './page/login/login.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
