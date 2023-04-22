7d6d62aa182cef905670bed93e3b4f4ce0cceca1
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-04-22 17:31:22 -0300

    added CoreModule to VisitorModule

--- src/app/modules/visitor/visitor.module.ts
+++ src/app/modules/visitor/visitor.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CoreModule } from '@core/core.module';
+
 import { VisitorRoutingModule } from './visitor-routing.module';
 import { VisitorComponent } from './page/visitor/visitor.component';
+
 
 import { ProfileComponent } from './components/profile/profile.component';
 import { LoginComponent } from './page/login/login.component';
@@ -25,6 +28,7 @@ import { RegisterComponent } from './page/register/register.component';
 		CommonModule,
 		VisitorRoutingModule,
 		FormsModule,
+		CoreModule,
 	]
 })
 export class VisitorModule { }
