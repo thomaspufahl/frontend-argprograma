efc65b40e19d4c3839661aea4ae62385f91ee9e4
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-04-23 12:47:12 -0300

    added feature logged validation for user page

--- src/app/core/guards/logged.guard.ts
+++ src/app/core/guards/logged.guard.ts
@@ -1,30 +1,41 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { TokenService } from '../services/token/token.service';
+import { TokenService, tokenGetter } from '../services/token/token.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class LoggedGuard implements CanActivate {
 
-	constructor(private readonly router: Router, private readonly tokenSvc: TokenService) { }
+	constructor(
+		private readonly router: Router,
+		private readonly jwtHelper: JwtHelperService,
+		private readonly tokenService: TokenService
+	) { }
 
 	canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-		if (this.isLogged()) {
-			return true;
+
+		let exitCondition = true;
+
+		if (!this.tokenService.existsToken()) { exitCondition = false; }
+		if (this.tokenService.isExpired()) { exitCondition = false; }
+		if (this.isLogged()) { exitCondition = true; }
+
+		if (!exitCondition) {
+			alert("You don't have permission to access this page! Please log in.");
+			this.router.navigate(['/']);
 		}
-		alert("You don't have permission to access this page! Please log in.");
-		this.router.navigate(['/']);
-		return false;
+		return exitCondition;
 	}
 
-	isLogged(): boolean {
-		if (this.tokenSvc.getAuthorities().toString() == ('ADMIN' || 'USER')) {
-			return true;
-		}
+	public isLogged() {
 
-		return false;
+		const token = tokenGetter();
+		const payLoad = this.jwtHelper.decodeToken(token!)
+
+		if (payLoad === null) { return false; }
+		return payLoad.logged;
 	}
-
 }
