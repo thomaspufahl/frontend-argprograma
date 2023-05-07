e5436c6467b53bcd399d35683423fe2665cb0e6f
thomaspufahl <thomas.r.pufahl@gmail.com>
2023-05-07 19:20:59 -0300

    updated api url auth

--- src/app/core/authentication/auth.service.ts
+++ src/app/core/authentication/auth.service.ts
@@ -3,15 +3,16 @@ import { Injectable } from '@angular/core';
 import { User } from '@models/user.model';
 import { Jwt } from '@models/jwt.model';
 import { Observable } from 'rxjs';
+import { ApiService } from '../http/api.service';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class AuthService {
 
-	authURL = 'http://localhost:8080/auth/';
+	authURL = this.api.getApiUrl() + '/auth/';
 
-	constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient, private readonly api: ApiService) { }
 
 	public register(user: User): Observable<Jwt> {
 		return this.http.post<Jwt>(this.authURL + 'register', {
